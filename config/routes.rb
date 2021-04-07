91b2e037bed80d67176eb2a5d909c79f0805eaf2
Kiana Long <kianajade42.kl@gmail.com>
2021-04-07 16:25:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :authors, only: [:show] do
     resources :posts, only: [:show, :index]
   end
-  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+end
   
